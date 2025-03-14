939598ee3bb5ea803d0ff06e53e59626c9bcf8bf
AleRicci26 <ale.2000.ar26@gmail.com>
2025-03-14 13:11:30 +0100

    docs: added gameobject documentation

--- src/main/java/clashclass/ecs/GameObject.java
+++ src/main/java/clashclass/ecs/GameObject.java
@@ -3,12 +3,33 @@ package clashclass.ecs;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Represents an entity, which acts like an "empty object" with only a unique id.
+ */
 public interface GameObject {
+    /**
+     * Gets the unique id.
+     * @return unique id
+     */
     public int getUniqueId();
 
+    /**
+     * Adds a component to the set of components.
+     * @param component component to add
+     */
     public void addComponent(Component component);
 
+    /**
+     * Tries to the get a component of a specific type.
+     * @param componentType type of the component to get, can be either a class or an interface
+     * @return component of the desired type, if exists, otherwise returns an empty Optional
+     * @param <T> the type of the component to get
+     */
     <T extends Component> Optional<T> getComponentOfType(Class<T> componentType);
 
+    /**
+     * Gets all the components
+     * @return all the components
+     */
     public Set<Component> getComponents();
 }
--- src/main/java/clashclass/ecs/GameObjectImpl.java
+++ src/main/java/clashclass/ecs/GameObjectImpl.java
@@ -5,32 +5,54 @@ import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
+/**
+ * Represents an implementation of GameObject
+ */
 public class GameObjectImpl implements GameObject {
     private final int uniqueId;
     private Set<Component> components;
 
     private static int uniqueIdProgression = 0;
 
+    /**
+     * Constructs the GameObject
+     */
     public GameObjectImpl() {
         this.uniqueId = uniqueIdProgression++;
         this.components = new HashSet<>();
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final int getUniqueId() {
         return this.uniqueId;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final void addComponent(Component component) {
         this.components.add(component);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public final <T extends Component> Optional<T> getComponentOfType(Class<T> componentType) {
-        return Optional.empty();
+        return this.components.stream()
+                .filter(componentType::isInstance)
+                .map(componentType::cast)
+                .findFirst();
     }
 
+    /**
+     * Gets an immutable representation of all the components
+     * @return an immutable representation of all the components
+     */
     @Override
     public final Set<Component> getComponents() {
         return Collections.unmodifiableSet(components);
