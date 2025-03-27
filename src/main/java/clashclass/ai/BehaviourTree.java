b82667da75129f279c0a99637ab4571ce1006f98
AleRicci26 <ale.2000.ar26@gmail.com>
2025-03-27 16:25:03 +0100

    [feat]: added behaviour tree component

---
+++ src/main/java/clashclass/ai/BehaviourTree.java
@@ -0,0 +1,11 @@
+package clashclass.ai;
+
+import clashclass.ecs.Component;
+import clashclass.ecs.UpdateProvider;
+
+/**
+ * Represents a Behaviour Tree.
+ */
+public interface BehaviourTree extends Component, UpdateProvider {
+
+}
---
+++ src/main/java/clashclass/ai/BehaviourTreeImpl.java
@@ -0,0 +1,16 @@
+package clashclass.ai;
+
+import clashclass.ecs.AbstractComponent;
+
+/**
+ * Represents a BehaviourTree implementation.
+ */
+public class BehaviourTreeImpl extends AbstractComponent implements BehaviourTree {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void update(final float deltaTime) {
+
+    }
+}
