a2daf4055e00aba3195960b52149ff83ead1f0ae
AleRicci26 <ale.2000.ar26@gmail.com>
2025-03-27 14:27:18 +0100

    [feat]: added battle troop factory

---
+++ src/main/java/clashclass/elements/BattleTroopFactoryImpl.java
@@ -0,0 +1,25 @@
+package clashclass.elements;
+
+import clashclass.commons.Vector2D;
+import clashclass.ecs.GameObject;
+
+/**
+ * Represents an implementation of TroopFactory used for battle.
+ */
+public class BattleTroopFactoryImpl extends AbstractTroopFactory {
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GameObject createBarbarian(Vector2D position) {
+        return super.createBarbarian(position);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public GameObject createArcher(Vector2D position) {
+        return super.createArcher(position);
+    }
+}
