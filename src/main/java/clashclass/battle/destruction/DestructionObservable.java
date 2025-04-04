b3e6e71ed4c2bbf625cc956ac2a3528fa0a9cb7e
larck <lollobulf@gmail.com>
2025-04-04 17:32:17 +0200

    [docs]: added partial documentation for DestructionObservable and DestructionObserver

--- src/main/java/clashclass/battle/destruction/DestructionObservable.java
+++ src/main/java/clashclass/battle/destruction/DestructionObservable.java
@@ -4,9 +4,16 @@ package clashclass.battle.destruction;
 //update va implementato all interno di destructionObservable
 
 public interface DestructionObservable {
-
+    /**
+     * Add an observer to the set of Observers
+     * @param observer to add
+     */
     void addObserver(DestructionObserver observer);
 
+    /**
+     * Remove an observer to the set of Observers
+     * @param observer to remove
+     */
     void removeObserver(DestructionObserver observer);
 
 }
--- src/main/java/clashclass/battle/destruction/DestructionObserver.java
+++ src/main/java/clashclass/battle/destruction/DestructionObserver.java
@@ -4,6 +4,10 @@ import clashclass.ecs.GameObject;
 
 public interface DestructionObserver {
 
+    /**
+     * Notify the destruction of a GameObject
+     * @param obj the object that has been destroyed
+     */
     void NotifyDestruction(GameObject obj);
 
 }
