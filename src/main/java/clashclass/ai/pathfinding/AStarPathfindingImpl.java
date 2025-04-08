9ff5c4da64e144bbc92768a0c84c57693868ef3a
AleRicci26 <ale.2000.ar26@gmail.com>
2025-04-08 22:52:52 +0200

    [feat]: Added initial A* Algorithm implementation

--- src/main/java/clashclass/ai/pathfinding/AStarPathfindingImpl.java
+++ src/main/java/clashclass/ai/pathfinding/AStarPathfindingImpl.java
@@ -1,11 +1,15 @@
 package clashclass.ai.pathfinding;
 
-import java.util.List;
+import java.nio.file.Path;
+import java.util.*;
 
 /**
  * Represents an implementation of A*, a well-know pathfinding algorithm.
+ * <a href="https://www.baeldung.com/java-a-star-pathfinding"/>
  */
 public class AStarPathfindingImpl implements PathfindingAlgorithm {
+    private final Queue<PathNode> openSet;
+    private final Set<PathNode> closedSet;
     private final DistanceHeuristic distanceHeuristic;
 
     /**
@@ -14,6 +18,8 @@ public class AStarPathfindingImpl implements PathfindingAlgorithm {
      * @param distanceHeuristic the heuristic to use for distance estimation
      */
     public AStarPathfindingImpl(final DistanceHeuristic distanceHeuristic) {
+        this.openSet = new PriorityQueue<>();
+        this.closedSet = new HashSet<>();
         this.distanceHeuristic = distanceHeuristic;
     }
 
@@ -22,6 +28,24 @@ public class AStarPathfindingImpl implements PathfindingAlgorithm {
      */
     @Override
     public List<PathNode> findPath(final List<PathNode> nodes, final PathNode start, final PathNode end) {
+        this.openSet.clear();
+        this.closedSet.clear();
+
+        this.openSet.add(start);
+
+        while (!this.openSet.isEmpty()) {
+            final PathNode next = this.openSet.poll();
+            if (next.equals(end)) {
+                List<PathNode> route = new ArrayList<>(nodes);
+                PathNode current = next;
+                do {
+                    route.add(0, current);
+//                    current = nodes.get(current.get)
+                } while(current != null);
+                return route;
+            }
+        }
+
         return List.of();
     }
 }
