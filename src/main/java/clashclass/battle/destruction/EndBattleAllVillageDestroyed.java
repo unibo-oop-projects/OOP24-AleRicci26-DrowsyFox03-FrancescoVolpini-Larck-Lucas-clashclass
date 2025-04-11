81a7706ad3306f6713b7931c1fde178f2fa47f81
larck <lollobulf@gmail.com>
2025-04-11 20:06:38 +0200

    [docs]: added documentation for EndBattleAllVillageDestroyedImpl and its interface

--- src/main/java/clashclass/battle/destruction/EndBattleAllVillageDestroyed.java
+++ src/main/java/clashclass/battle/destruction/EndBattleAllVillageDestroyed.java
@@ -1,6 +1,11 @@
 package clashclass.battle.destruction;
-
+/**
+ * This class represent one possible cause of the end of the battle.
+ * In this case the complete destruction of the enemy base.
+ */
 public interface EndBattleAllVillageDestroyed extends DestructionObserver{
-
+   /**
+    * Verify if the village is fully destroyed.
+    */
    boolean isFullyDestroyed();
 }
--- src/main/java/clashclass/battle/destruction/EndBattleAllVillageDestroyedImpl.java
+++ src/main/java/clashclass/battle/destruction/EndBattleAllVillageDestroyedImpl.java
@@ -3,19 +3,31 @@ package clashclass.battle.destruction;
 import clashclass.ecs.AbstractComponent;
 import clashclass.ecs.GameObject;
 
+/**
+ * Implementation of EndBattleAllVillageDestroyed interface
+ */
 public class EndBattleAllVillageDestroyedImpl extends AbstractComponent implements EndBattleAllVillageDestroyed {
 
     private boolean isFullyDestroyed;
 
+    /**
+     * Initialize the flag of the village to not destroyed
+     */
     EndBattleAllVillageDestroyedImpl(){
         this.isFullyDestroyed=false;
     }
 
+    /**
+     *{@inheritDoc}
+     */
     @Override
     public boolean isFullyDestroyed() {
         return BattleReportController.getPercentage()==100?true:false;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void notifyDestruction(GameObject obj) {
         if(isFullyDestroyed()){
@@ -23,5 +35,4 @@ public class EndBattleAllVillageDestroyedImpl extends AbstractComponent implemen
             AbstractEndBattleEvent.EndBattle();
         }
     }
-
 }
