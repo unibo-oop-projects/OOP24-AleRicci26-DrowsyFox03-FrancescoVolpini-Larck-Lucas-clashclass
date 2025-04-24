64583252a29f5e98e01e17375e2ef6c9b4145ff0
Francesco Volpini <francesco.volpini2@studio.unibo.it>
2025-04-24 12:01:35 +0200

    [doc]: added ResourceManager documentation

--- src/main/java/clashclass/resources/Player.java
+++ src/main/java/clashclass/resources/Player.java
@@ -1,8 +1,18 @@
 package clashclass.resources;
 
+/**
+ * Class that provide the player with different resources.
+ */
 public class Player {
-    private final int GENERIC_VALUE = 10;
-    public ResourceManagerImpl gold = new ResourceManagerImpl(GENERIC_VALUE);
-    public ResourceManagerImpl elixir = new ResourceManagerImpl(GENERIC_VALUE);
-    public ResourceManagerImpl gems = new ResourceManagerImpl(GENERIC_VALUE);
+    private static final int GENERIC_VALUE = 10;
+    public enum ResourceType {
+        GOLD,
+        ELIXIR,
+        GEMS;
+
+        public final ResourceManagerImpl resource;
+        ResourceType(){
+            resource = new ResourceManagerImpl(GENERIC_VALUE);
+        }
+    }
 }
--- src/main/java/clashclass/resources/ResourceManagerImpl.java
+++ src/main/java/clashclass/resources/ResourceManagerImpl.java
@@ -1,27 +1,43 @@
 package clashclass.resources;
 
+/**
+ * Represents a ResourceManager implementation.
+ */
 public class ResourceManagerImpl implements ResourceManager {
     private final int MAX_VALUE;
     private int currentValue;
 
+    /**
+     * Construct ResourceManager.
+     *
+     * @param MAX_VALUE the maximum value of the resource
+     */
     public ResourceManagerImpl(final int MAX_VALUE) {
         this.MAX_VALUE = MAX_VALUE;
         this.currentValue = MAX_VALUE;
     }
+
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void increase(int value) {
         this.currentValue = Math.min(MAX_VALUE, this.currentValue - value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void decrease(int value) {
         this.currentValue = Math.max(0, this.currentValue - value);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public int getCurrentValue() {
         return this.currentValue;
     }
-
-
 }
