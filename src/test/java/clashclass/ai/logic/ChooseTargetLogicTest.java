ab805821a8e6ab88db256319a17c449e13c30b9e
AleRicci26 <ale.2000.ar26@gmail.com>
2025-04-28 00:21:50 +0200

    [test]: added basic test for Choose Target Logic

---
+++ src/test/java/clashclass/ai/logic/ChooseTargetLogicTest.java
@@ -0,0 +1,35 @@
+package clashclass.ai.logic;
+
+import clashclass.commons.Transform2D;
+import clashclass.commons.Vector2D;
+import clashclass.ecs.GameObjectImpl;
+import clashclass.elements.buildings.BattleBuildingFactoryImpl;
+import clashclass.elements.troops.BattleTroopFactoryImpl;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ChooseTargetLogicTest {
+
+    @Test
+    void TestChooseNearestTarget() {
+        final var chooseTargetLogic = new ChooseTargetNearestLogicImpl();
+        final var buildingsFactory = new BattleBuildingFactoryImpl();
+        final var troopFactory = new BattleTroopFactoryImpl();
+
+        final var building1 = buildingsFactory.createCannon(new Vector2D(1, 2));
+        final var building2 = buildingsFactory.createCannon(new Vector2D(1, 3));
+        final var building3 = buildingsFactory.createCannon(new Vector2D(2, 0));
+
+        final var targetsList = List.of(building1, building2, building3);
+
+        final var troop = troopFactory.createBarbarian(new Vector2D(0, 0));
+
+        final var nearestTarget = chooseTargetLogic.chooseTarget(troop, targetsList);
+
+        assertEquals(new Vector2D(1, 2), nearestTarget.getComponentOfType(Transform2D.class).get().getPosition());
+    }
+}
