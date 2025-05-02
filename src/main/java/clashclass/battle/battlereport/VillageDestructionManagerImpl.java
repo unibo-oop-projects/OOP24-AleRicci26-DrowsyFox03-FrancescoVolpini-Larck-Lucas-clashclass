56eada2c4f67d609449561eab8f569b7c1771798
larck <lollobulf@gmail.com>
2025-05-02 17:36:38 +0200

    [refactor]: changed name of the class from VillageDestructionManager to VillageDestructionManagerImpl to avoid ambiguity

--- src/main/java/clashclass/battle/battlereport/VillageDestructionManagerImpl.java
+++ src/main/java/clashclass/battle/battlereport/VillageDestructionManagerImpl.java
@@ -1,19 +1,18 @@
 package clashclass.battle.battlereport;
 
-import clashclass.battle.destruction.DestructionObserver;
 import clashclass.ecs.AbstractComponent;
 import clashclass.ecs.GameObject;
 
 
-public class VillageDestructionManager extends AbstractComponent implements DestructionObserver {
-    
-    private final BattleReportController battleReportController;
-    
+public class VillageDestructionManagerImpl extends AbstractComponent implements VillageDestructionManager {
 
-    public VillageDestructionManager(final BattleReportController battleReportController) {
+    private final BattleReportController battleReportController;
+
+
+    public VillageDestructionManagerImpl(final BattleReportController battleReportController) {
         this.battleReportController = battleReportController;
     }
-    
+
 
     @Override
     public void notifyDestruction(final GameObject obj) {
