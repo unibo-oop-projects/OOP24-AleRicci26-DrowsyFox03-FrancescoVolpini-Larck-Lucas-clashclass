04d63fb587aeabde7d54fe73c15a5aa7f76e8c4e
larck <lollobulf@gmail.com>
2025-05-05 16:31:18 +0200

    [docs]: added the documentation for battlereport controller interface

--- src/main/java/clashclass/battle/battlereport/BattleReportController.java
+++ src/main/java/clashclass/battle/battlereport/BattleReportController.java
@@ -1,10 +1,28 @@
 package clashclass.battle.battlereport;
 
+/**
+ * Interface for the Battle Report Controller.
+ * Manages the battle report data including destruction percentage and stolen resources.
+ */
 public interface BattleReportController {
 
+    /**
+     * Increases the destruction percentage of the village.
+     * This is called when a building is destroyed.
+     */
     void increaseDestructionPercentage();
 
+    /**
+     * Increases the amount of stolen resources.
+     *
+     * @param resourceManager The resource manager containing the resources to be added
+     */
     void increaseStolenResources(ResourceManager resourceManager);
 
+    /**
+     * Sets the total number of troops used in the battle.
+     *
+     * @param count The total number of troops used
+     */
     void setTroopCount(int count);
 }
