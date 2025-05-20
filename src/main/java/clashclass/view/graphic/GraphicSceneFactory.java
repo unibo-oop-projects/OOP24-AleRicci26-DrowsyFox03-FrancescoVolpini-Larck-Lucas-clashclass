165510e07685588bcb3498b0232d97c707fc7076
Francesco Volpini <francesco.volpini2@studio.unibo.it>
2025-05-20 12:55:12 +0200

    creation of the GUI using javafx stage

---
+++ src/main/java/clashclass/view/graphic/GraphicSceneFactory.java
@@ -0,0 +1,14 @@
+package clashclass.view.graphic;
+
+/**
+ * Factory interface for creating scenes in the application.
+ */
+public interface GraphicSceneFactory {
+
+    /**
+     * Creates and returns a new BaseScene representing the menu.
+     * @param window the window context in which the menu scene will be used
+     * @return a BaseScene instance representing the menu
+     */
+    BaseScene createMenuScene(Window window);
+}
