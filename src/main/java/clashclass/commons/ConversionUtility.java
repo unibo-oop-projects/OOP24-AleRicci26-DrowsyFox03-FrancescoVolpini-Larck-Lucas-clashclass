d21287c5c78b4a5922b8f49dd173fd7156e00427
AleRicci26 <ale.2000.ar26@gmail.com>
2025-06-04 08:32:38 +0200

    [fix]: issue with position world to grid re-conversion

--- src/main/java/clashclass/commons/ConversionUtility.java
+++ src/main/java/clashclass/commons/ConversionUtility.java
@@ -5,7 +5,7 @@ public class ConversionUtility {
         final var centerGridPosition = gridPosition;
         final double step = GameConstants.TILE_PIXEL_SIZE * (GameConstants.TILE_SCALE / 2.0);
         final double screenOffsetX = GameConstants.SCREEN_WIDTH / 2.0;
-        final double screenOffsetY = -(GameConstants.SCREEN_HEIGHT / 2.0) * (GameConstants.TILE_SCALE / 2.0);
+        final double screenOffsetY = -((double)GameConstants.SCREEN_HEIGHT * 0.7) * (GameConstants.TILE_SCALE / 2.0);
         return new Vector2D(
                 (centerGridPosition.x() - centerGridPosition.y()) * step + screenOffsetX,
                 (centerGridPosition.x() + centerGridPosition.y()) * step + screenOffsetY
@@ -15,13 +15,13 @@ public class ConversionUtility {
     public static VectorInt2D convertWorldToGridPosition(final Vector2D worldPosition) {
         final double step = GameConstants.TILE_PIXEL_SIZE * (GameConstants.TILE_SCALE / 2.0);
         final double screenOffsetX = GameConstants.SCREEN_WIDTH / 2.0;
-        final double screenOffsetY = -(GameConstants.SCREEN_HEIGHT / 2.0) * (GameConstants.TILE_SCALE / 2.0);
+        final double screenOffsetY = -((double) GameConstants.SCREEN_HEIGHT * 0.7) * (GameConstants.TILE_SCALE / 2.0);
 
-        double wx = worldPosition.x() - screenOffsetX;
-        double wy = worldPosition.y() - screenOffsetY;
+        double isoX = (worldPosition.x() - screenOffsetX) / step;
+        double isoY = (worldPosition.y() - screenOffsetY) / step;
 
-        int gridX = (int) Math.floor((wx + wy) / (2 * step));
-        int gridY = (int) Math.floor((wy - wx) / (2 * step));
+        int gridX = (int) Math.floor((isoY + isoX) / 2);
+        int gridY = (int) Math.floor((isoY - isoX) / 2);
 
         return new VectorInt2D(gridX, gridY);
     }
--- src/main/java/clashclass/commons/GameConstants.java
+++ src/main/java/clashclass/commons/GameConstants.java
@@ -2,7 +2,7 @@ package clashclass.commons;
 
 public class GameConstants {
     public static final int VILLAGE_SIZE = 40;
-    public static final double TILE_SCALE = 0.6f;
+    public static final double TILE_SCALE = 0.7f;
     public static final int TILE_PIXEL_SIZE = 22;
     public static final int SCREEN_WIDTH = 600;
     public static final int SCREEN_HEIGHT = 400;
