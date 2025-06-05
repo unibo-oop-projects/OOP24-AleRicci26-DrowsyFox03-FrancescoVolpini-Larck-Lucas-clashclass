f96a9cdf91668fce501bd93838c41894ea3bab96
AleRicci26 <ale.2000.ar26@gmail.com>
2025-06-05 14:41:37 +0200

    [feat]: added battle report javaFX implementation

---
+++ src/main/java/clashclass/battle/battlereport/BattleReportViewJavaFXImpl.java
@@ -0,0 +1,35 @@
+package clashclass.battle.battlereport;
+
+import clashclass.resources.ResourceManager;
+
+public class BattleReportViewJavaFXImpl implements BattleReportView {
+    @Override
+    public void update(BattleReportModel model) {
+
+    }
+
+    @Override
+    public void displayDestructionPercentage(double percentage) {
+
+    }
+
+    @Override
+    public void displayStars(int stars) {
+
+    }
+
+    @Override
+    public void displayStolenResources(ResourceManager resources) {
+
+    }
+
+    @Override
+    public void displayBattleResult(boolean isVictory) {
+
+    }
+
+    @Override
+    public void displayTroopCount(int troopCount) {
+
+    }
+}
