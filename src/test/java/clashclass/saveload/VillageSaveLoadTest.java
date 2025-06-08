cbb99565fe7317bb614d3b281900af3f4cc2f69c
Lucas <lucasantonio.leonte@studio.unibo.it>
2025-06-08 16:42:06 +0200

    Ã[test]: old mock test for village save-load moved to VillageMockTest

--- src/test/java/clashclass/saveload/VillageSaveLoadTest.java
+++ src/test/java/clashclass/saveload/VillageSaveLoadTest.java
@@ -1,15 +1,10 @@
 package clashclass.saveload;
 
 import clashclass.commons.BuildingTypeComponent;
-import clashclass.commons.VectorInt2D;
-import clashclass.ecs.GameObject;
 import clashclass.elements.ComponentFactoryImpl;
-import clashclass.elements.buildings.*;
-import clashclass.resources.Player;
-import clashclass.village.Village;
+import clashclass.elements.buildings.VillageElementData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.io.TempDir;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -17,58 +12,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class VillageSaveLoadTest {
 
-    @TempDir
-    Path tempDir;
-
-    private VillageSaveLoadManager saveLoadManager;
-    private FileWriter fileWriter;
-    private VillageEncoder encoder;
     private PlayerVillageDecoderImpl playerDecoder;
-    private BattleVillageDecoderImpl battleDecoder;
-    private BuildingFactoryMapper<PlayerBuildingFactoryImpl> playerMapper;
-    private BuildingFactoryMapper<BattleBuildingFactoryImpl> battleMapper;
-
 
     @BeforeEach
     void setUp() {
-        fileWriter = new SimpleFileWriterImpl();
-        encoder = new VillageEncoderImpl();
-
-        PlayerBuildingFactoryImpl playerBuildingFactory = new PlayerBuildingFactoryImpl();
-        BattleBuildingFactoryImpl battleBuildingFactory = new BattleBuildingFactoryImpl();
-
         playerDecoder = new PlayerVillageDecoderImpl();
         playerDecoder.setComponentFactory(new ComponentFactoryImpl());
-
-        battleDecoder = new BattleVillageDecoderImpl();
-        battleDecoder.setComponentFactory(new ComponentFactoryImpl());
-
-        playerMapper = new BuildingFactoryMapper<>(playerBuildingFactory);
-        battleMapper = new BuildingFactoryMapper<>(battleBuildingFactory);
-
-        saveLoadManager = new VillageSaveLoadManager(
-                encoder,
-                playerDecoder,
-                battleDecoder,
-                fileWriter,
-                tempDir
-        );
     }
-
     @Test
-    void testFileNotFound() {
-        String nonExistentFile = "does_not_exist";
-
-        // Test both loading methods
-        assertThrows(IOException.class, () ->
-                saveLoadManager.loadPlayerVillage(nonExistentFile));
-
-        assertThrows(IOException.class, () ->
-                saveLoadManager.loadBattleVillage(nonExistentFile));
-    }
-
-    @Test
-    void loadVillageFromResources() throws IOException {
+    void loadPlayerVillageFromResources() throws IOException {
         Path resourcePath = Path.of("src/main/resources/Villages-Data/player-village.csv");
         String csv = Files.readString(resourcePath);
 
